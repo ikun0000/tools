fa15c3a54a4b42702b6c6ec2961cb14a26a3c008
kun <32808774+zikunchen@users.noreply.github.com>
2019-01-29 20:09:49 +0800

    Update tcpserver.py

--- tcpserver.py
+++ tcpserver.py
@@ -5,10 +5,12 @@ bind_ip='0.0.0.0'
 bind_port=9999
 
 server=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# TCP服务端要使用bind()指定监听地址和端口，并且使用listen()设置最大连接数
 server.bind((bind_ip, bind_port))
 server.listen(5)
 print '[*] Listening on %s:%d' % (bind_ip, bind_port)
 
+# 线程函数
 def handle_client(client_socket):
     request=client_socket.recv(4096)
     print '[*] Received: %s' % request
@@ -16,6 +18,9 @@ def handle_client(client_socket):
     client_socket.close()
 
 while True:
+    # 之后使用accept()进入监听并返回链接的实例和地址端口信息
+    # addr[0] : client ip address
+    # addr[0] : client port
     client,addr=server.accept()
     print '[*] Accept connection from %s:%d' % (addr[0], addr[1])
     client_handler=threading.Thread(target=handle_client,args=(client,))
