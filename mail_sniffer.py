aecd1b6bfaa419f98d4ef83295203a8c1aeb3ffb
kun <32808774+zikunchen@users.noreply.github.com>
2019-02-01 13:43:10 +0800

    Update mail_sniffer.py

--- mail_sniffer.py
+++ mail_sniffer.py
@@ -1,5 +1,6 @@
 from scapy.all import *
 
+# 数据包回调函数
 def packet_callback(packet):
     if packet[TCP].payload:
 	mail_packet=str(packet[TCP].payload)
@@ -8,4 +9,5 @@ def packet_callback(packet):
 	    print "[+] Server: %s " % packet[IP].dst
 	    print "[+] %s " % packet[TCP].payload
 
+# 开启嗅探器
 sniff(filter="tcp port 110 or tcp port 25 or tcp port 143", prn=packet_callback, store=0)
